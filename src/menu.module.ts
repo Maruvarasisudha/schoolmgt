7117cba6594e0b54137086c703d10c2f247f6ede
Maruvarasisudha <111410670+Maruvarasisudha@users.noreply.github.com>
2022-11-08 09:41:21 +0530

    first commit

---
+++ src/menu.module.ts
@@ -0,0 +1,10 @@
+
+export interface MenuItem {
+    id?: any;
+    label?: string;
+    icon?: string;
+    link?: string;
+    subItems?: any;
+    parentId?: number;
+    isUiElement?: boolean;
+}
---
+++ src/menu.ts
@@ -0,0 +1,27 @@
+import { MenuItem } from "./menu.module";
+export const MENU: MenuItem[] = [
+  
+    {
+        id: 1,
+        label: 'Home',
+        link: '/dashboard',
+    }, {
+        id: 2,
+        label: 'About',
+        link: '/dashboard',
+    
+    }, {
+        id: 3,
+        label: 'course',
+        link: '/dashboard',
+    },
+    {
+        id: 4,
+        label: 'News',
+        link: '/dashboard',
+    }, {
+        id: 5,
+        label: 'Contact',
+        link: '/dashboard',
+    },
+]
